5240f0ba20619d786bcd27b7282904c46d686a6a
Mathieu Audebert <matheosmat@gmail.com>
2017-10-14 15:43:43 +0200

    indexs

---
+++ app/controllers/vehicule_items_controller.rb
@@ -0,0 +1,17 @@
+class VehiculeItemsController < ApplicationController
+  def index
+
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  resources :vehicules, only: [:index, :show]
+  resources :vehicule_items, only: [:index, :new, :create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
